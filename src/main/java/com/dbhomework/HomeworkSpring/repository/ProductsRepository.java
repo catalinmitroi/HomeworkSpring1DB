209ee6227d901108e2ea85326d7714babed5162d
catalinmitroi <catalinmitroi18@yahoo.com>
2022-08-03 10:35:33 +0300

    Added

--- src/main/java/com/dbhomework/HomeworkSpring/repository/ProductsRepository.java
+++ src/main/java/com/dbhomework/HomeworkSpring/repository/ProductsRepository.java
@@ -3,6 +3,9 @@ package com.dbhomework.HomeworkSpring.repository;
 import com.dbhomework.HomeworkSpring.model.Products;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProductsRepository extends CrudRepository<Products, Integer> {
         public void delete(Products product);
+        public Optional<Products> findById(Integer id);
 }
